41f058a1feb10041124e18ff17f899e845cd5ce2
Jean-Philippe Eisenbarth <jpeisenbarth@users.noreply.github.com>
2018-03-21 13:08:26 +0100

    [update, fix] License

--- test/helper.js
+++ test/helper.js
@@ -1,19 +1,19 @@
-// Copyright 2017 Jean-Philippe Eisenbarth
+// Copyright 2017-2018 Jean-Philippe Eisenbarth
 //
 // This file is part of Crypto API wrapper.
 //
-// Foobar is free software: you can redistribute it and/or modify
+// Crypto API Wrapper is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Mute Authentication Proxy is distributed in the hope that it will be useful,
+// Crypto API Wrapper is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with Foobar. See the file COPYING.  If not, see <http://www.gnu.org/licenses/>.
+// along with Crypto API Wrapper. See the file COPYING.  If not, see <http://www.gnu.org/licenses/>.
 
 const encoder = new TextEncoder()
 const decoder = new TextDecoder()
